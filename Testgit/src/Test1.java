dfedaac08490bbd0b9f57302078ccee813612d48
David346204131 <34182594+David346204131@users.noreply.github.com>
2017-12-02 17:54:34 +0800

    Update Test1.java

--- Testgit/src/Test1.java
+++ Testgit/src/Test1.java
@@ -1,9 +1,6 @@
 
 public class Test1 {
 
-	public void test1() {
-		System.out.println("推送到远程");
-	}
 
 
 
@@ -11,7 +8,10 @@ public class Test1 {
 		System.out.println("提交到本地test2");
 	}
 
-
+	
+	public void test22() {
+		System.out.println("提交到本地test2");
+	}
 	public void test3() {
 		System.out.println("提交到本地");
 	}
@@ -20,13 +20,12 @@ public class Test1 {
 		System.out.println("推送到远程");
 	}
 
-	public void test5() {
-		System.out.println("推送到远程");
-	}
 
 	public void test6() {
 		System.out.println("推送到远程1");
 	}
-	
+		public void test7() {
+		System.out.println("推送到远程1");
+	}
 
 }
