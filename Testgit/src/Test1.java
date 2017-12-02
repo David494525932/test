6a9ab2d78a44cf6b1b48b9700961371635ea2183
David494525932 <33303437+David494525932@users.noreply.github.com>
2017-12-02 16:04:14 +0800

    Update Test1.java

--- Testgit/src/Test1.java
+++ Testgit/src/Test1.java
@@ -1,6 +1,14 @@
 
 public class Test1 {
-	public static void main(String[] args) {
-		System.out.println("12313213");
+
+
+	public void test(){
+	
+	System.out.println("12313213");
+	}
+	
+	public void test1(){
+	
+	System.out.println("12313213");
 	}
 }
