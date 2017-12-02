9719c8ff3f1069a4cb592d2cb9ce6156849eea74
David346204131 <34182594+David346204131@users.noreply.github.com>
2017-12-02 17:18:03 +0800

    Update Test1.java

--- Testgit/src/Test1.java
+++ Testgit/src/Test1.java
@@ -2,14 +2,15 @@
 public class Test1 {
 
 
-
-	public void test11() {
-		System.out.println("没有提交到本地");
-	}
-	
-	public void test111() {
+	public void test1() {
 		System.out.println("没有提交到本地test11");
 	}
+	
+	public void test11() {
+		System.out.println("没有提交到本地test11");
+	}
+	
+
 
 	public void test2() {
 		System.out.println("提交到本地");
@@ -22,9 +23,7 @@ public class Test1 {
 	public void test33() {
 		System.out.println("推送到远程");
 	}
-	public void test333() {
-		System.out.println("推送到远程");
-	}
+
 
 
 }
