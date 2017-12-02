ef2146bd555555b839901248512915be521fa305
David <David494525932@gmail.com>
2017-12-02 16:37:34 +0800

    adaa

--- Testgit/src/Test1.java
+++ Testgit/src/Test1.java
@@ -6,25 +6,6 @@ public class Test1 {
 
 	
 
-	public void test1(){
-	
-	System.out.println("david1修改123113");
-	}
-
-	public void test11(){
-		
-		System.out.println("davi113111113");
-		}
-
-	public void test2(){
-	
-	System.out.println("1231231113213");
-	}
-
-	public void test3(){
-	
-	System.out.println("1231231113213");
-	}
 
 
 }
