75919bb68bef5f540bda0c42b613848593f3e441
David <David494525932@gmail.com>
2017-12-02 19:04:51 +0800

    萨达

--- Testgit/src/Test1.java
+++ Testgit/src/Test1.java
@@ -1,16 +1,12 @@
 
 public class Test1 {
 
-
-
-
-	public void test2() {
+	public void test1() {
 		System.out.println("提交到本地test2");
 	}
 
 
-	
-	public void test22() {
+	public void test2() {
 		System.out.println("提交到本地test2");
 	}
 
@@ -23,21 +19,6 @@ public class Test1 {
 	}
 
 
-	public void test5() {
-		System.out.println("推送到远程");
-	}
 
-	public void test6() {
-		System.out.println("推送到远程1");
-	}
-
-	public void test7() {
-	System.out.println("推送到远程1");
-}
-	public void test8() {
-
-
-		System.out.println("推送到远程1");
-	}
 
 }
