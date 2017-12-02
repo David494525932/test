313ad3cd68dee4ee2916bb5957bc0590c9f62a3a
David <David494525932@gmail.com>
2017-12-02 16:25:33 +0800

    12131

--- Testgit/src/Test1.java
+++ Testgit/src/Test1.java
@@ -7,11 +7,11 @@ public class Test1 {
 	
 	public void test1(){
 	
-	System.out.println("david2修改");
+	System.out.println("david1修改123113");
 	}
 		
-	public void test2(){
-	
-	System.out.println("1231231113213");
-	}
+	public void test11(){
+		
+		System.out.println("davi113111113");
+		}
 }
