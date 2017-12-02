7d85ec91de2853b2394536570d1f18cfaff7ceb2
David346204131 <34182594+David346204131@users.noreply.github.com>
2017-12-02 16:19:04 +0800

    Update Test1.java

--- Testgit/src/Test1.java
+++ Testgit/src/Test1.java
@@ -5,12 +5,21 @@ public class Test1 {
 
 
 	
-	public void test1(){
-	
-	System.out.println("david2修改");
-	}
+
 		
 	public void test2(){
+	
+	System.out.println("1231231113213");
+	}
+	public void test2(){
+	
+	System.out.println("1231231113213");
+	}
+	public void test3(){
+	
+	System.out.println("1231231113213");
+	}
+	public void test4(){
 	
 	System.out.println("1231231113213");
 	}
