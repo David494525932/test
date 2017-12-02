06c33234ca470382822a44829978554f520bd7ea
David <David494525932@gmail.com>
2017-12-02 17:48:12 +0800

    123

--- Testgit/src/Test1.java
+++ Testgit/src/Test1.java
@@ -1,22 +1,22 @@
 
 public class Test1 {
 
-
 	public void test1() {
-		System.out.println("没有提交到本地test1111");
+		System.out.println("推送到远程");
+	}
+	public void test2() {
+		System.out.println("推送到远程");
 	}
 	
-
-
-	public void test2() {
-		System.out.println("提交到本地test2");
-	}
-
 	public void test3() {
-		System.out.println("推送到远程test3");
+		System.out.println("提交到本地");
 	}
 
 	public void test4() {
+		System.out.println("推送到远程");
+	}
+
+	public void test5() {
 		System.out.println("推送到远程");
 	}
 
